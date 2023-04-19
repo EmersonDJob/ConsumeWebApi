7d9b88686ac29b25deb3db89a7ed152299b1cec9
EmersonDJob <jobbr70@gmail.com>
2023-04-19 12:25:54 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using ConsumeWebApi.Interface;
+using ConsumeWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ConsumeWebApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly IApiConnect    _connect;
+
+        public HomeController(ILogger<HomeController> logger, IApiConnect apiConnect)
+        {
+            _logger = logger;
+            _connect = apiConnect;
+        }
+
+        public  async Task<IActionResult> Index()
+        {
+            var getAll = await _connect.ConnectAsync();
+            return View(getAll);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Interface/IApiConnect.cs
@@ -0,0 +1,11 @@
+﻿using ConsumeWebApi.Models;
+using Refit;
+
+namespace ConsumeWebApi.Interface
+{
+    public interface IApiConnect
+    {
+        [Get("/posts")]
+        Task<List<SamplePost>> ConnectAsync();
+    }
+}
